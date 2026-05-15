5a1d48cdeaabce4e8850a6e706654385f5d55d49
Fiza Shahid <fiza34037@gmail.com>
2026-05-15 10:49:06 +0500

    updating ui

--- app/welcome/layout.tsx
+++ app/welcome/layout.tsx
@@ -1,23 +1,23 @@
-// app/welcome/layout.tsx
-import Navbar from '../../components/Navbar';
-import React from 'react';
-import Home from '../../../myapp/app/page';
-import Hero from '../../components/Hero';
+// // app/welcome/layout.tsx
+// import Navbar from '../../components/Navbar';
+// import React from 'react';
+// import Home from '../../../myapp/app/page';
+// import Hero from '../../components/Hero';
 
-export default function WelcomeLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen flex flex-col items-center bg-gray-50">
-      {/* Optional: Header specific to welcome pages */}
-      <header >
+// export default function WelcomeLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <div className="min-h-screen flex flex-col items-center bg-gray-50">
+//       {/* Optional: Header specific to welcome pages */}
+//       <header >
       
-      </header>
+//       </header>
 
-      <main className="flex-1 w-full max-w-4xl p-4">
-        {children}  
+//       <main className="flex-1 w-full max-w-4xl p-4">
+//         {children}  
         
-      </main>
+//       </main>
 
     
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
